32870ed1ecf6efdea52ba1775a45c12ff8f10f9e
Ashwin1729 <mathematicswroteus@gmail.com>
2023-08-15 00:49:10 +0530

    footer implemented

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,7 +16,7 @@ const Footer = () => {
             style="flex items-center justify-center p-[16px] rounded-[10px] w-[130px] h-[51px] [background:linear-gradient(90.38deg,_#ffac75,_#fe6468_74.48%,_#ad71f2)] font-kamerik-105 font-[700] text-[16px] text-white cursor-pointer"
           />
         </div>
-        <div className="flex flex-col justify-between items-start w-1/2 h-full px-14 py-10">
+        <div className="flex flex-col justify-between items-start w-1/2 h-full px-14 py-5">
           <div>
             <div className="font-[700] text-[32px] text-white">Menu</div>
             <div className="font-[700] text-lg text-[#CBBFAC] my-1 cursor-pointer">
